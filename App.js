406e926e318d472947e2bdaf809da0e610d13a46
Samuel Lefcourt <samasherlefcourt@aol.com>
2018-01-06 14:58:48 -0800

    Sign up page for parents

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import App2 from './containers/app.js'
+import styles from './styles.js'
+import RootNavigation from './navigation/navigator';
 
-export default class App extends React.Component {
+export default class MyApp extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <RootNavigation />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
